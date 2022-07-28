dbf151c7e91d56a9a3f3b1b51232b5d11d14006c
hiren patel <hirenpatel0526@yahoo.com>
2022-07-27 19:25:32 -0700

    <arrays>

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+/*
+var topic = "HTML";
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git!') {
+    console.log("LeT's study Git");
+} else if (topic === 'javaScript!') {
+    console.log("Let's study Javascript!");
+} else {
+    console.log('Please try again!');
+}
+*/
